b4dfce800bda39dea8b42c7c42a92366224cf1f5
jpmeagher <J.Meagher@warwick.ac.uk>
2022-04-12 16:15:58 +0100

    simulate hawkes cluster

---
+++ tests/testthat/test-simulate_clusters.R
@@ -0,0 +1,33 @@
+testthat::test_that("Simulated Hawkes process satisfies sanity checks", {
+  set.seed(1011)
+  tst <- simulate_hawkes_cluster(
+    t_i = 0, reproduction_number = 0.99, gi_exp_decay_rate = 1, horizon = 2
+  )
+  N <- nrow(tst)
+  testthat::expect_true(
+    all(tst$t[1:(N-1)] < tst$t[2:N])
+  )
+  testthat::expect_true(
+    all(tst$id > tst$parent_id)
+  )
+  testthat::expect_error(
+    simulate_hawkes_cluster(
+      t_i = 4, reproduction_number = 0.5, gi_exp_decay_rate = 1, horizon = 2
+    )
+  )
+  testthat::expect_warning(
+    simulate_hawkes_cluster(
+      t_i = -1, reproduction_number = 1, gi_exp_decay_rate = 1, horizon = 2
+    )
+  )
+  testthat::expect_error(
+    simulate_hawkes_cluster(
+      t_i = 0, reproduction_number = 0.5, gi_exp_decay_rate = -1, horizon = 2
+    )
+  )
+  testthat::expect_error(
+    simulate_hawkes_cluster(
+      t_i = 0, reproduction_number = -1, gi_exp_decay_rate = 10, horizon = 2
+    )
+  )
+})
