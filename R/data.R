510035765c1b34da9ad8960b6d830c8abcbd1dae
jpmeagher <J.Meagher@warwick.ac.uk>
2022-10-21 15:14:15 +0100

    update datasets

--- R/data.R
+++ R/data.R
@@ -2,7 +2,7 @@
 #'
 #' A dataset containing timestamps and branching structure for activity on the
 #' \eqn{r/ireland} subreddit for the 42 days (6 weeks) starting on April 1,
-#' 2019
+#' 2019. Consists of 117,787 events within 38,616 converstaions.
 #'
 #' @format A dataframe with 117,787 observations of 4 variables:
 #' \describe{
@@ -13,25 +13,29 @@
 #'}
 "messageboard_df"
 
-#' Training data for modelling online message-board activity
+#' Training data for models of online message-board activity
 #'
-#' A dataset containing discussion trees on the \eqn{r/ireland} subreddit
-#' seeded between April 1 and April 28, 2019 and observed up until April 28,
-#' 2019. Each seeded tree is included with probability \eqn{p = 0.1}.
+#' A dataset containing discussion trees on the \eqn{r/ireland} subreddit seeded
+#' by posts between April 1 and April 28, 2019 and observed for 48 hours after the
+#' initial post. Each discussion seeded during the period is included with
+#' probability \eqn{p = 0.1} for a total of 704 discussions consisting of 2262
+#' events.
 #'
-#' @format A dataframe with 8,108 observations of 4 variables:
-#' \describe{
-#'   \item{id}{A unique, positive integer id for each event ordered by timestamp.}
-#'   \item{parent_id}{The id for the parent of each event. Set to 0 when the event is a post.}
-#'   \item{tree}{The id for each distinct discussion tree seeded by a post.}
-#'   \item{time}{The timestamp for each event.}
-#'}
+#' @format A dataframe with 8,108 observations of 4 variables: \describe{
+#'  \item{id}{A unique, positive integer id for each event ordered by
+#'  timestamp.} \item{parent_id}{The id for the parent of each event. Set to 0
+#'  when the event is a post.} \item{tree}{The id for each distinct discussion
+#'  tree seeded by a post.} \item{time}{The timestamp for each event.} }
 "train_df"
 
-#' Testing data for modelling online message-board activity
+#' Testing data models od online message-board activity
 #'
 #' A dataset containing discussion trees on the \eqn{r/ireland} subreddit
-#' seeded between April 29 and May 12, 2019 and observed up until May 12, 2019.
+#' seeded between April 29 and May 10, 2019 and observed for 48 hours after the
+#' initial post. Initially, each discussion seeded during the period is included with
+#' probability \eqn{p = 0.1}, but those that had already appeared in the training data are
+#' then excluded. This provides a total of 3,601 discussions consisting of 10,864
+#' events.
 #'
 #' @format A dataframe with 37,2022 observations of 4 variables:
 #' \describe{
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,10 +1,11 @@
-## code to prepare `DATASET` dataset goes here
+## code to prepare data sets goes here
 
 ## Packages used
 library(magrittr)
 library(future.apply)
 library(dplyr)
 library(lubridate)
+library(onlineMessageboardActivity)
 
 ## Specify that futures will be resolved in parallel
 plan("multisession", workers = 6)
@@ -16,12 +17,13 @@ raw_df <- read.csv("~/R/Projects/reddit_data/data/ireland_SR_1549670400_15812064
   mutate(parent = future_sapply(strsplit(Parent, "_"), function(x) x[2])) %>%
   select(time = Time, type, id = ID, parent)
 
-## Explicitly identify posts and comments to improve clarity
-new_type <- rep("comment", nrow(raw_df))
-new_type[raw_df$type == "t3"] <- "post"
-raw_df <- raw_df %>%
-  mutate(type = factor(new_type, levels = c("post", "comment")))
-rm(new_type)
+# ## Explicitly identify posts and comments to improve clarity
+# new_type <- rep("comment", nrow(raw_df))
+# new_type[raw_df$type == "t3"] <- "post"
+# raw_df <- raw_df %>%
+#   mutate(type = factor(new_type, levels = c("post", "comment")))
+# rm(new_type)
+
 
 ## Specify data for analysis
 messageboard_df <- raw_df %>%
@@ -30,7 +32,7 @@ messageboard_df <- raw_df %>%
     parent_id = refactor_branching_structure(
       id = id,
       parent_id = parent,
-      is_immigrant = (type == "post"),
+      is_immigrant = (type == "t3"),
       S = 2000
     )) %>%
   mutate(id = 1:nrow(.)) %>%
@@ -48,48 +50,74 @@ messageboard_df <- raw_df %>%
 
 usethis::use_data(messageboard_df, overwrite = TRUE)
 
+## Training data
+max_tau <- 48
+set.seed(123456)
 
-## train/test split
-p <- 0.1
-set.seed(101)
-train_trees <- messageboard_df %>%
-  filter(time < dmy(29042019)) %>%
+S_training_total <- messageboard_df %>%
+  filter(time < dmy(08042019)) %>%
   use_series(tree) %>%
   unique() %>%
-  length() %>%
-  rbinom(size = 1, p = p) %>%
+  as.numeric() %>%
+  max()
+
+training_discussions <- rbinom(S_training_total, 1, p = 0.1) %>%
   as.logical() %>%
   which()
 
+S_training <- length(training_discussions)
+
 train_df <- messageboard_df %>%
-  filter(time < dmy(29042019)) %>%
-  filter(tree %in% train_trees) %>%
+  filter(tree %in% training_discussions) %>%
   mutate(
     parent_id = refactor_branching_structure(
-      id = id,
-      parent_id = parent_id,
-      is_immigrant = (parent_id == 0),
-      S = 2000
-    )) %>%
-  mutate(id = 1:nrow(.))
+      id = id, parent_id = parent_id,
+      is_immigrant = parent_id == 0,
+      S = 250
+    )
+  ) %>%
+  mutate(id = seq_along(id)) %>%
+  mutate(t = as.numeric(difftime(time, dmy_hms(010419000000, tz = "Europe/London"), units = "hours"))) %>%
+  group_by(tree) %>%
+  mutate(tau = t - min(t)) %>%
+  ungroup() %>%
+  filter(tau < max_tau) %>%
+  select(id, parent_id, tree, t)
 
 usethis::use_data(train_df, overwrite = TRUE)
 
-test_trees <- messageboard_df %>%
-  filter(time >= dmy(29042019)) %>%
-  filter(parent_id == 0) %>%
+## Testing Data
+set.seed(1234567)
+
+S_testing_total <- messageboard_df %>%
+  filter(time < dmy(11052019)) %>%
   use_series(tree) %>%
-  as.numeric()
+  unique() %>%
+  as.numeric() %>%
+  max()
+
+tmp_testing_discussions <- rbinom(S_testing_total, 1, p = 0.1) %>%
+  as.logical()
+tmp_testing_discussions[training_discussions] <- FALSE
+testing_discussions <- tmp_testing_discussions %>%
+  which()
+S_testing <- length(testing_discussions)
 
 test_df <- messageboard_df %>%
-  filter(tree %in% test_trees) %>%
+  filter(tree %in% testing_discussions) %>%
   mutate(
     parent_id = refactor_branching_structure(
-      id = id,
-      parent_id = parent_id,
-      is_immigrant = (parent_id == 0),
-      S = 2000
-    )) %>%
-  mutate(id = 1:nrow(.))
+      id = id, parent_id = parent_id,
+      is_immigrant = parent_id == 0,
+      S = 250
+    )
+  ) %>%
+  mutate(id = seq_along(id)) %>%
+  mutate(t = as.numeric(difftime(time, dmy_hms(010419000000, tz = "Europe/London"), units = "hours"))) %>%
+  select(id, parent_id, tree, t) %>%
+  group_by(tree) %>%
+  mutate(tau = t - min(t)) %>%
+  ungroup() %>%
+  filter(tau < max_tau)
 
 usethis::use_data(test_df, overwrite = TRUE)
